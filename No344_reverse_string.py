290d43097f755fd28496e06c6d262775429c26d6
Hornbill201 <Hornbill@Chaos-MacBook-Pro.local>
2016-08-28 18:15:42 -0400

    Add N292 N344

---
+++ No344_reverse_string.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 28 17:20:37 2016
+
+@author: Hornbill
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
+        
